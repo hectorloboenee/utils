37931b5da0e56e820529ecf51268f3435f357e3d
hector.lobo10000 <hector.lobo10000@gmail.com>
2023-04-18 16:00:31 -0600

    agregando script

---
+++ script-multi-tabs-bash.sh
@@ -0,0 +1,12 @@
+gnome-terminal --full-screen \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+  --tab --working-directory=YOUR_DIRECTORY --title=TAB_TITLE \
+
+## add alias in .bashrc
+## alias pdh="./script-multi-tabs-bash.sh"
